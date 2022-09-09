96fc1603d2eaf87ab10ca795251c466b76a7e99f
CuzinVin <vps108@gmail.com>
2022-09-09 19:30:32 -0400

    image revision to work with build

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,18 @@ import React from 'react'
 import Intro from './Intro'
 import Quiz from './Quiz'
 import './App.css'
+import yellowblob from './assets/yellowblob.svg'
+import blueblob from './assets/blueblob.svg'
 
 function App() {
   const [startQ, setStartQ] = React.useState(true)
 
   return (
-    startQ ? <Intro handleClick={setStartQ} /> : <Quiz />
+    <div>
+      <div className="rtblob" style={{backgroundImage: `url(${yellowblob})`}}></div>
+      <div className="blblob" style={{backgroundImage: `url(${blueblob})`}}></div>
+      {startQ ? <Intro handleClick={setStartQ} /> : <Quiz />}
+    </div>
   )
 }
 
