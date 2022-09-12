b5f49b0daa81f73eae14090ab94fced9ad5acbfc
CuzinVin <vps108@gmail.com>
2022-09-12 11:27:07 -0400

    more special characters fixed

--- src/Question.jsx
+++ src/Question.jsx
@@ -11,13 +11,13 @@ export default function Question(props) {
         return (
             <li key={Ans}>
                 <input onChange={() => props.handleClick(Ans, props.correct, props.group)} className="ans" type="radio" id={props.newID[v]} name={props.group} value={Ans} />
-                <div className={newClass}><label htmlFor={props.newID[v]}>{Ans.replace(/&quot;/g, '"').replace(/&#039;/g, "'").replace(/&amp;/g, "&").replace(/&egrave;/g, "è").replace(/&rdquo;/g, '"').replace(/&ldquo;/g, '"').replace(/&aacute;/g, 'á').replace(/&ntilde;/g, 'ñ')}</label></div>
+                <div className={newClass}><label htmlFor={props.newID[v]}>{Ans.replace(/&quot;/g, '"').replace(/&#039;/g, "'").replace(/&amp;/g, "&").replace(/&egrave;/g, "è").replace(/&rdquo;/g, '"').replace(/&ldquo;/g, '"').replace(/&aacute;/g, 'á').replace(/&ntilde;/g, 'ñ').replace(/&oacute;/g, 'ó').replace(/&eacute;/g, 'é')}</label></div>
             </li>
         )
     })
     return (
         <div className="qSection">
-            <h4>{props.quest.replace(/&quot;/g, '"').replace(/&#039;/g, "'").replace(/&amp;/g, "&").replace(/&egrave;/g, "è").replace(/&rdquo;/g, '"').replace(/&ldquo;/g, '"').replace(/&aacute;/g, 'á').replace(/&ntilde;/g, 'ñ')}</h4>
+            <h4>{props.quest.replace(/&quot;/g, '"').replace(/&#039;/g, "'").replace(/&amp;/g, "&").replace(/&egrave;/g, "è").replace(/&rdquo;/g, '"').replace(/&ldquo;/g, '"').replace(/&aacute;/g, 'á').replace(/&ntilde;/g, 'ñ').replace(/&oacute;/g, 'ó').replace(/&eacute;/g, 'é')}</h4>
             <ul className="answerHolder">
                 {finalAs}
             </ul>
